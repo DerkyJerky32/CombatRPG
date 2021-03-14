70735c6c8ec02edca417ee56fe63d543480f1f12
Derrick Demers <demersderrick@yahoo.com>
2021-03-13 19:56:48 -0600

    Combat skeleton for the Capstone game

---
+++ RPGCombat/Assets/Scripts/FighterAction.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class FighterAction : MonoBehaviour
+{
+    private GameObject enemy;
+    private GameObject hero;
+
+    [SerializeField]
+    private GameObject meleePrefab;
+
+    [SerializeField]
+    private GameObject rangePrefab;
+
+    [SerializeField]
+    private Sprite faceIcon;
+
+    private GameObject currentAttack;
+
+    void Awake()
+    {
+        hero = GameObject.FindGameObjectWithTag("Hero");
+        enemy = GameObject.FindGameObjectWithTag("Enemy");
+    }
+    
+    public void SelectAttack(string btn)
+    {
+        GameObject victim = hero;
+
+        if(tag == "Hero")
+        {
+            victim = enemy;
+        }
+
+        if(btn.CompareTo("melee") == 0)
+        {
+            meleePrefab.GetComponent<AttackScript>().Attack(victim);
+        }
+
+        else if (btn.CompareTo("range") == 0)
+        {
+            rangePrefab.GetComponent<AttackScript>().Attack(victim);
+        }
+
+        else
+        {
+            Debug.Log("Run!!");
+        }
+
+    }
+
+}
